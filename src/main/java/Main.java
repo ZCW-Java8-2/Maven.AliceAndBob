3d4707f9ffeafb598f4b8ded60bfbc47f278ba57
Tarin <tarin@Zipcoders-MacBook-Pro.local>
2022-11-01 21:28:43 -0400

    Fiinished lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+
+        System.out.println("What is your name");
+        // Create new scanner object called inputObject.
+        Scanner inputObject =  new Scanner(System.in);
+        // Create new variable "name" and stores the user input "inputObject"
+        String name = inputObject.nextLine();
+        // Prints the output "Your name is" and includes the variable "username" which is any name.
+        System.out.println("Your name is " + name + ".");
+
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("Sorry, " + name + " was not found.");
+        }
+
+
+
+
+
+
 
     }
 }
